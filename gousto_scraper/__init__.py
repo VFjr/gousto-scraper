d6e606aa41647bdc5ca63f72ba91755c5e077e94
Victor <victor@engineeredarts.co.uk>
2024-11-25 16:47:59 +0000

    Prepare structure for library

---
+++ gousto_scraper/__init__.py
@@ -0,0 +1,3 @@
+from .scraper import add
+
+__all__ = ['add']
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gousto_scraper',
+    version='0.0.1',
+    description='Gousto recipe scraper',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/VFjr/gousto-scraper',
+    author='Victor Florea',
+    author_email='victor.florea.jr@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
